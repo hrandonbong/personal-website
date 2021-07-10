54e1113a7e05974efc542f4a944e560ede13b6fa
hrandonbong <71863517+hrandonbong@users.noreply.github.com>
2021-07-10 12:03:28 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+// app.js
+var express = require('express');
+
+// Define Express App
+var app = express();
+//var port = process.env.PORT || 8080;
+app.set('port', 7106);
+
+// Server Static Assets
+app.use(express.static('public'));
+
+//Virtual Path Prefix '/static'
+app.use('/static', express.static('public'))
+
+
+//app.listen(app.get('port'), function(){
+  //console.log(`Express started on  http://${process.env.HOSTNAME}:${app.get('port')} press Ctrl-C to terminate.`);
+//});
+
+app.listen(app.get('port'), function(){
+    console.log('Express started on  http://localhost:' + app.get('port') + ' press Ctrl-C to terminate.');
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Set up Routes
+//app.get('/', function(req, res) {
+    //res.sendFile(path.join(__dirname,'index.html'));
+////});
+
+//app.get('/favorite', function(req, res) {
+   // res.sendFile(path.join(__dirname, 'favorite.html'));
+//});
+
+
+
+
+
+
+
+
+
